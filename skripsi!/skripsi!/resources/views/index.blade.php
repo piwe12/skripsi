4df6156d4dcb67ad1fb01ce0700171b9e39b7ab6
Hafidh Zulfiqar <72957325+hafidhzulfiqar@users.noreply.github.com>
2022-04-17 15:01:55 +0700

    1

---
+++ skripsi!/skripsi!/resources/views/index.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="{{ URL('assets/favicon.ico') }}">
+
+    <title>Purchash</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/pricing/">
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/carousel/">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+    <!-- Bootstrap core CSS -->
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
+    <link href="{{ URL('css-js/css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="{{ URL('css-js/css/pricing.css') }}" rel="stylesheet">
+    <link href="{{ URL('css-js/css/navbar.css') }}" rel="stylesheet">
+    <link href="{{ URL('css-js/css/carousel.css') }}" rel="stylesheet">
+
+    hallo
+</head>
+
+
+
+
+</html>
